954e32b2bd798ec275ce1d5066d0373137fd74a9
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2021-12-20 21:48:24 +0530

    Create Pattern Syntax Checker.java

---
+++ Java/Pattern Syntax Checker.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases > 0){
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+}
+/*
+Sample Input
+
+3
+([A-Z])(.+)
+[AZ[a-z](a-z)
+batcatpat(nat
+
+
+Sample Output
+
+Valid
+Invalid
+Invalid
+*/
