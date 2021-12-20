c2a8b08575ce5e8c64d707c4e69542f1f76d4aeb
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2021-12-20 21:46:31 +0530

    Create Java String Tokens.java

---
+++ Java/Java String Tokens.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+        public static void main(String[] args) 
+        {
+        Scanner scan = new Scanner(System.in);
+        String s=scan.nextLine();
+        //Complete the code
+            if (s.trim().length()==0 || s.trim().length()>400000)
+            {
+                System.out.println(0);
+                return;
+            }
+
+            String words[]=s.trim().split("[ !,?.\\_'@]+");
+            System.out.println(words.length);
+            for (String word:words)
+                System.out.println(word);
+        }
+}
+
+/*
+Sample Input
+
+He is a very very good boy, isn't he?
+
+Sample Output
+
+10
+He
+is
+a
+very
+very
+good
+boy
+isn
+t
+he
+
+Explanation
+
+We consider a token to be a contiguous segment of alphabetic characters. 
+There are a total of 10 such tokens in string s, and each token is printed in the same order in which it appears in string s.
+*/
