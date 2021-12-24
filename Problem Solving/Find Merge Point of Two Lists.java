e76b0fe8be08cfba5820d8307c95d3b696741b50
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2021-12-24 18:34:52 +0530

    Create Find Merge Point of Two Lists.java

---
+++ Problem Solving/Find Merge Point of Two Lists.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode t1 = head1;
+        SinglyLinkedListNode t2 = head2;
+        while(t1 != t2){
+            if(t1.next == null){
+                t1 = head2;
+            }else{
+                t1 = t1.next;
+            }
+            if(t2.next == null){
+                t2 = head1;
+            }else{
+                t2 = t2.next;
+            }
+        }
+        return t2.data;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
