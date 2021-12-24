1fe02ca5c4b4b0ee74d7adaf73606d58133382f3
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2021-12-24 18:26:55 +0530

    Create Delete duplicate-value nodes from a sorted linked list.java

---
+++ Problem Solving/Delete duplicate-value nodes from a sorted linked list.java
@@ -0,0 +1,151 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class SinglyLinkedListNode {
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    public SinglyLinkedListNode head;
+    public SinglyLinkedListNode tail;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertNode(int nodeData) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+}
+
+class SinglyLinkedListPrintHelper {
+    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'removeDuplicates' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head) {
+    // Write your code here
+    SinglyLinkedListNode dummy = new SinglyLinkedListNode(-(int)1e9);
+    SinglyLinkedListNode curr = head ;
+    SinglyLinkedListNode ne = dummy ;
+    
+    while(curr != null){
+        
+        while( curr != null && curr.data == ne.data ){
+            curr = curr.next;
+        }
+        ne.next = curr;
+        ne = ne.next; 
+        if(curr != null) curr = curr.next;
+    }
+    return dummy.next;
+    
+     
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                SinglyLinkedList llist = new SinglyLinkedList();
+
+                int llistCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+                IntStream.range(0, llistCount).forEach(i -> {
+                    try {
+                        int llistItem = Integer.parseInt(bufferedReader.readLine().trim());
+
+                        llist.insertNode(llistItem);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                SinglyLinkedListNode llist1 = Result.removeDuplicates(llist.head);
+
+                SinglyLinkedListPrintHelper.printList(llist1, " ", bufferedWriter);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+/*
+Sample Input
+
+STDIN   Function
+-----   --------
+1       t = 1
+5       n = 5
+1       data values = 1, 2, 2, 3, 4
+2
+2
+3
+4
+Sample Output
+
+1 2 3 4 
+*/
